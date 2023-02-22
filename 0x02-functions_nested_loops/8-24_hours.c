08844fcd9980d296385c801b77ca595c5b353597
2xkay <maureen.takedi@gmail.com>
2023-02-22 11:30:36 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,15 +7,14 @@ void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0; <= 23; hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
 
 		_putchar((hour / 10) + '0');
 		_putchar((hour % 10) + '0');
 		_putchar(':');
-		_putchar((miute / 10) + '0');
+		_putchar((minute /10) + '0');
 		_putchar((minute % 10) + '0');
-		_putchar('/n');
+		_putchar('\n');
 	}
 }
-
